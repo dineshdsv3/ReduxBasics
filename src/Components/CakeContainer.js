85e08bacb85aba7630c36e1e9330bd024d2aa1fe
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-11 00:14:17 +0530

    Added actions, reducers, connect to the code

---
+++ src/Components/CakeContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { buyCake } from '../Redux';
+import { connect } from 'react-redux';
+
+const  CakeContainer = (props) => {
+	return (
+		<div>
+			<h2>Cake container {props.noOfCakes}</h2>
+			<button onClick={props.buyCake}>Buy cake</button>
+		</div>
+	);
+}
+
+const mapStateToProps = (state) => {
+	return {
+		noOfCakes: state.noOfCakes,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		buyCake: () => dispatch(buyCake()),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
