b31b5ba7598d6e3a3b59a33478d99f0195ea476b
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-10 22:47:34 +0530

    Added react packages

---
+++ asyncActions.js
@@ -0,0 +1,85 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default;
+const axios = require('axios');
+
+// Initial State
+const initialState = {
+	loading: false,
+	users: [],
+	error: '',
+};
+
+// Constants
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+// Action Creators
+const fetchUsersRequest = () => {
+	return {
+		type: FETCH_USERS_REQUEST,
+	};
+};
+
+const fetchUsersSuccess = (users) => {
+	return {
+		type: FETCH_USERS_SUCCESS,
+		payload: users,
+	};
+};
+
+const fetchUsersFailure = (error) => {
+	return {
+		type: FETCH_USERS_FAILURE,
+		payload: error,
+	};
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case FETCH_USERS_REQUEST: {
+			return {
+				...state,
+				loading: true,
+			};
+		}
+		case FETCH_USERS_SUCCESS: {
+			return {
+				loading: false,
+				users: action.payload,
+				error: '',
+			};
+		}
+		case FETCH_USERS_FAILURE: {
+			return {
+				loading: false,
+				users: [],
+				error: action.payload,
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+// Async action creator - fetch users
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequest());
+        axios.get('https://jsonplaceholder.typicode.co/users')
+        .then((res) => {
+            const users = res.data.map(user => user.id);
+            dispatch(fetchUsersSuccess(users))
+        })
+        .catch(error => {
+            dispatch(fetchUsersFailure(error.message))
+        })
+    }
+}
+
+const store = createStore(reducer,applyMiddleware(thunkMiddleware));
+store.subscribe(()=>console.log(store.getState()))
+store.dispatch(fetchUsers());
