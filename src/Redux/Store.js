8c640ce59877620b940bc92dfca5a4045e17bad9
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-11 01:49:12 +0530

    Added devtools

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 
 export default store;
--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import iceCreamReducer from './Icecream/iceCreamReducer';
 import cakeReducer from './Cakes/cakeReducer';
 import icecreamReducer from './Icecream/iceCreamReducer';
 
